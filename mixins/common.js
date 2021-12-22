1841e46333a42276228a03894244780cda410a15
anggitrisi <anggitrisihombing@gmail.com>
2021-12-22 21:52:41 +0700

    edit common.js

--- mixins/common.js
+++ mixins/common.js
@@ -1,8 +1,29 @@
 const Validator = require("fastest-validator");
+const jwt = require('jsonwebtoken');
+const userRepository = require("../domains/users/repository");
 
 module.exports = {
   name: "common",
-  before: (ctx) => {
+  before: async (ctx) => {
+    // verify auth
+    if (ctx.action.authentication === true) {
+      let token = null;
+      try {
+        token = ctx.headers.authorization.split(" ")[1];
+      } catch (error) {
+        throw new Error("token not found");
+      }
+      if (!token) {
+        throw new Error("token not found");
+      }
+      try {
+        const decodedJwt = jwt.verify(token, process.env.JWT_TOKEN_SECRET);
+        ctx.user = await userRepository.getById(decodedJwt.sub);
+      } catch (error) {
+        throw new Error("token invalid");
+      }
+    }
+
     // validator
     if (ctx.action.validator && typeof ctx.action.validator === "object") {
       const validate = new Validator().compile(ctx.action.validator);
