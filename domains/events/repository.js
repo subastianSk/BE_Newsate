6b05b4e40c36e4f81c6fbb289becf40b9f3f6c9e
unknown <zulfikar.fauzul.akbar@gmail.com>
2021-12-23 21:16:11 +0700

    events endpoint done

--- domains/events/repository.js
+++ domains/events/repository.js
@@ -2,11 +2,22 @@ const db = require("../../models");
 
 module.exports = {
   getById: async (id) => {
-    const result = await db.events.findByPk(id);
+    const result = await db.events.findByPk(id, {
+      include: [
+        {
+          model: db.participants,
+          attributes: ["id", "email"],
+        },
+        {
+          model: db.users,
+          attributes: ["id", "email"]
+        }
+      ],
+    });
     return result;
   },
   create: async (payload) => {
-    const result = await db.participants.create(payload);
+    const result = await db.events.create(payload);
     return result;
-  }
+  },
 };
--- domains/events/service.js
+++ domains/events/service.js
@@ -2,6 +2,12 @@ const common = require("../../mixins/common");
 const repository = require("./repository");
 
 
+function compare( a, b ){
+    if(a.point > b.point)
+        return -1;
+    return 0;
+}
+
 module.exports = {
     name: "events",
     mixins: [common],
@@ -14,12 +20,7 @@ module.exports = {
             responseMessage: "success create events",
             handler: async (ctx) => {
                 const payload = ctx.payload.body;
-                const newPayload = {
-                    ...payload,
-                    date: new Date(),
-                    createdBy: ctx.user.id
-                }
-                const result = await repository.create(newPayload);
+                const result = await repository.create(payload);
                 return result;
             },
         },
@@ -27,12 +28,31 @@ module.exports = {
             responseMessage: "success get data event",
             method: "get",
             path: "/:id",
-            handler: (ctx) => {
-                const event = repository.getById(Number(ctx.payload.params.id));
+            handler: async (ctx) => {
+                const event = await repository.getById(Number(ctx.payload.params.id));
                 if (!event) {
                     throw new Error("id not exist");
                 }
-                return event;
+
+                console.log(event);
+
+                const participants = event.participants.map((o) => ({
+                    id: o.id,
+                    email : o.email,
+                    point : o.participants_points.point
+                }));
+
+                participants.sort(compare);
+
+                const result = {
+                    id : event.id,
+                    name : event.name,
+                    date : event.date,
+                    creted_by : event.user,
+                    leaderboard : participants.slice(0,3)
+                };
+
+                return result;
             },
         },
     },
